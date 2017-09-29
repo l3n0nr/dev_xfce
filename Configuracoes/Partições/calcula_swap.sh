f32bdecb2827752582f2d685a53e4060980793de
lenonr <lenonrmsouza@gmail.com>
2017-09-29 16:54:07 -0300

    arquivo formatado, codigo identado, funcao funcionaod

--- Configuracoes/Partições/calcula_swap.sh
+++ Configuracoes/Partições/calcula_swap.sh
@@ -1,53 +1,114 @@
-# Referência: <http://sejalivre.org/entendendo-as-variaveis-em-shell-script/>
+#!/bin/bash
+# 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+ 
+#                           CABEÇALHO DO SCRIPT                               #
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+# 
+######################
+# FONTES DE PESQUISA #
+######################
+#
+################################################################################
+# Referência: <http://sejalivre.org/entendendo-as-variaveis-em-shell-script/>
+################################################################################
+#
+####################
+# DESENVOLVIDO POR #
+####################
+#
+# por lenonr(Lenon Ricardo) 
+#       contato: <lenonrmsouza@gmail.com>
+#
+#################################################################################
+#										#
+#	If I have seen further it is by standing on the shoulders of Giants.	#
+#	(Se vi mais longe foi por estar de pé sobre ombros de gigantes)		#
+#							~Isaac Newton		#
+#										#
+#################################################################################
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # 
+# # versão do script:           [0.0.5.0.0.0]    #
+# # data de criação do script:    [29/09/17]      #
+# # ultima ediçao realizada:      [29/09/17]      #
+# # # # # # # # # # # # # # # # # # # # # # # # # # 
+#
+# Legenda: a.b.c.d.e.f
+# 	a = alpha[0], beta[1], stable[2], freeze[3];
+# 	b = erros na execução;	
+# 	c = interações com o script;
+# 	d = correções necessárias;
+# 	e = pendencias    
+# 	f = desenvolver
+#
+################################################################################
+#
+# # Script testado em
+#	- Xubuntu 16.04
+#       - Debian 9
+#
+# # Compativel com
+#       - Ubuntu
+#       - Debian 9
+# 
+################################################################################
+# # FUNCOES
+# 
+# # # Calcula a quantidade de memória, de acordo com a máquina.
+# 
+################################################################################
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+ 
+#                           CORPO DO SCRIPT                                   #
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 
 clear
-echo "CALCULADO DE MEMÓRIA SWAP"
-echo 
-
+echo "##############################################################"
+echo "CALCULANDO DE MEMÓRIA SWAP"
+echo "##############################################################"
 # capturando a quantidade de memoria
 tam_mem=$(free -mt | grep Mem: | awk '{ print $2 " MB" }' | sed -e "s; MB;;")
 
 # mostrando memoria instalada
-echo "Tamanho da memória é $tam_mem MB"
+echo "Tamanho da memória do seu computador é $tam_mem MB."
 
-# se valor menor que 2GB
-if [ "$tam_mem" -lt "2048" ]; then
-    # calculando tamanho swap
-    swap=$((($tam_mem * 5)/10))
+# # # # # # # # # # # # # # # # # #
+# TABELA DE APOIO PARA CONVERSAO  #
+# # # # # # # # # # # # # # # # # #
+# 
+# 5 == 6  | [ 5 -eq 6 ]
+# 5 != 6  | [ 5 -ne 6 ]
+# 5 < 6   | [ 5 -lt 6 ]
+# 5 <= 6  | [ 5 -le 6 ]
+# 5 > 6   | [ 5 -gt 6 ]
+# 5 >= 6  | [ 5 -ge 6 ]
+# 
+# # # # # # # # # # # # # # # # # 
 
-    # mostrando valor
-    echo "Tamanho da memória SWAP recomendada: $swap MB"	        
+# # realizando calculos
+    # se valor for menor ou igual a 2GB
+    if [ "$tam_mem" -le "2048" ]; then
+        # aplicando taxa de calculo, baseando em 50% da memória real
+        swap=$((($tam_mem * 5)/10))
+
+    # se valor for menor ou igual a 4GB
+    elif [ "$tam_mem" -lt "4092" ]; then
+        # aplicando taxa de calculo, baseando em 30% da memória real
+        swap=$((($tam_mem * 3)/10))    
+        
+    # qual outra opção 
+    else
+        # aplicando taxa de calculo, baseando em 10% da memória real
+        swap=$((($tam_mem * 1)/10))    
+    fi
     
-    echo     
-    # realizando soma das memorias
-    total=$(($tam_mem+$swap))
-    
-    # mostrando memoria total do computador
-    echo "O seu computador ira conter $total MB de memória total."
-    echo 
-
-elif [ "$tam_mem" -lt "4092" ]; then
-    # calculando tamanho swap
-    swap=$((($tam_mem * 3)/10))
-
-    # mostrando valor
-    echo "Tamanho da memória SWAP recomendada: $swap MB"
-
-    echo     
-    # realizando soma das memorias
-    total=$(($tam_mem+$swap))
-    
-    # mostrand memoria total do computador
-    echo "O seu computador ira conter $total MB de memória total."
-    echo 
-    
-else
-    # calculando tamanho swap
-    swap=$((($tam_mem * 1)/10))
-
-    # mostrando valor
-    echo "Tamanho da memória SWAP recomendada: $swap MB"
+    # mostrando valores
+    echo
+    echo "Tamanho da memória SWAP recomendada: $swap MB."
     
     echo     
     # realizando soma das memorias
@@ -55,6 +116,12 @@ else
     
     # mostrand memoria total do computador
     echo "O seu computador ira conter $total MB de memória total."
-    echo 
-fi
+    echo    
+    
+echo "##############################################################"
 
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+ 
+#                           RODAPE DO SCRIPT                                  #
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
