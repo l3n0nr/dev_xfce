bd22215ebb5029d4052c394731cf0f43e749f7b8
lenonr <lenonrmsouza@gmail.com>
2017-10-12 09:41:27 -0300

    funcao icones melhorada

--- Auto_Config/v4.sh
+++ Auto_Config/v4.sh
@@ -51,7 +51,7 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # 
-# # versão do script:           [0.0.147.2.0.4]   #
+# # versão do script:           [0.0.150.2.0.4]   #
 # # data de criação do script:    [28/09/17]      #
 # # ultima ediçao realizada:      [10/10/17]      #
 # # # # # # # # # # # # # # # # # # # # # # # # # # 
@@ -1412,13 +1412,36 @@ home = "/home/lenonr"
     icones()
     {
         printf "\n"
-        printf "[+] Instalando os icones \n"
-        
-        # copiando arquivos para as pastas
-        printf "[*] Movendo icones para pasta \n"
-        cp -r base/icons/* /usr/share/icons
-        
-        printf "[+] Arquivos movidos! \n"
+        printf "[+] Realizando backup dos arquivos \n"
+
+        # criando variaveis
+        # # variaveis de verificao
+        caminho='/home/lenonr/MEGA/Outros/Themes_Icons'
+        verifica='cd $caminho; echo $?'
+
+        # # variaveis de entrada
+        input_icons='/home/lenonr/MEGA/Outros/Themes_Icons/icons/*'
+        input_themes='/home/lenonr/MEGA/Outros/Themes_Icons/themes/*'
+
+        # # variaveis de saida
+        output_icons='/usr/share/icons/'
+        output_themes='/usr/share/themes/'
+
+        # realizando verificação na pasta de origem
+        if [[ ! -e $caminho ]]; then
+            # mostrando mensagem de erro
+            printf "[-] Caminho não encontrado!! \n"
+            printf "[-] Verifique a pasta de origem!! \n"
+            exit 127
+        else
+            # executando backup
+            printf "[+] Executando backup! \n"        
+            printf "[*] Copiando temas para pasta de destino \n"
+            cp -r $input_themes $output_themes
+            
+            printf "[*] Copiando icones para pasta de destino \n"
+            cp -r $input_icons $output_icons
+        fi
     }       
     
     brightside()
