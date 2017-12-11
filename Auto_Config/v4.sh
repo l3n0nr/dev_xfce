423820f5360aa04e736f5416df007fe43c82d42e
lenonr <lenonrmsouza@gmail.com>
2017-12-10 23:09:02 -0200

    funcao redshift melhorada

--- Auto_Config/v4.sh
+++ Auto_Config/v4.sh
@@ -51,9 +51,9 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # #
-# # versão do script:           [0.1.275.3.0.0]   #
+# # versão do script:           [0.1.276.3.0.0]   #
 # # data de criação do script:    [28/09/17]      #
-# # ultima ediçao realizada:      [07/12/17]      #
+# # ultima ediçao realizada:      [10/12/17]      #
 # # # # # # # # # # # # # # # # # # # # # # # # # #
 #
 # Legenda: a.b.c.d.e.f
@@ -879,6 +879,9 @@ func_help()
         printf "[+] Instalando o Redshift \n"
 
         apt install redshift gtk-redshift -y
+
+        # criando link
+        cat base/redshift.conf > pasta_home/.config/redshift.conf -f
     }
 
     libreoffice()
@@ -1624,11 +1627,13 @@ func_help()
         printf "\n"
         printf "[+] Instalando o Sublime \n"
 
-        wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add - 
-        sudo apt-get install apt-transport-https -y 
-        echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
-        sudo apt-get update  
-        sudo apt-get install sublime-text -y ;
+        wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add - 
+        apt install apt-transport-https -y 
+        echo "deb https://download.sublimetext.com/ apt/stable/" | tee /etc/apt/sources.list.d/sublime-text.list
+        
+        update  
+        
+        apt install sublime-text -y ;
     }
 
 # # # # # # # # # #
