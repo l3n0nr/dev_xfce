b6ddb5993788381cb501e751f62e2dcf8ddfeedd
Lenon Ricardo <lenonr@desktop>
2018-05-17 22:58:48 -0300

    zsh melhorado

--- Auto_Config/config_user.sh
+++ Auto_Config/config_user.sh
@@ -3,22 +3,37 @@
 # por lenonr(Lenon Ricardo)
 #       contato: <lenonrmsouza@gmail.com>
 #
+# variaveis
+var_zsh=$(which zsh)
+
+## REDSHIFT
+printf "\n[*] Copiando configuracao padrao do Redshift\n"
+cat base/redshift.conf > $HOME/.config/redshift.conf 
+
 ## ZSH
-printf "Entrando na pasta do usuario"
+if [[ ! -e $var_zsh ]]; then
+	printf "\n[-] ZSH nao esta instalado!\n"
+else	
+	printf "\n[*] Alterando bash padrao\n"	
+	chsh -s /bin/zsh
+fi
+
 printf "\n[*] Copiando arquivo padrao ZSH\n"
 cat base/.zshrc > $HOME/.zshrc
 
-printf "\nBaixando zsh-autosuggestions\n"
-# git clone https://github.com/zsh-users/zsh-autosuggestions ~/$HOME/.zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
+if [ -e "$HOME/.zsh/zsh-autosuggestions" ]; then
+	printf "\nVoce ja possui o zsh-autosuggestions"
+else
+	printf "\n[+] Baixando zsh-autosuggestions\n"
+	git clone https://github.com/zsh-users/zsh-autosuggestions $HOME/.zsh/zsh-autosuggestions
+fi
 
 if [ -e "$HOME/.oh-my-zsh" ]; then
 	printf "\nVoce ja possui o Oh-my-zsh"
 else
-	printf "\nBaixando oh-my-zsh"
+	printf "\n[+] Baixando oh-my-zsh"
+	printf "\n[*] Entrando na pasta do usuario"
 	cd $HOME
 	sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 fi
 
-## REDSHIFT
-cat base/redshift.conf > $HOME/.config/redshift.conf 
