64f4e20ee5f127df3c3da7837b0258b3cdff7a75
Lenon Ricardo <lenonr@desktop>
2018-05-18 09:47:42 -0300

    arquivos melhorados

---
+++ Auto_Config/base/debian/keys.sh
@@ -0,0 +1,10 @@
+############ KEYS
+apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys FC918B335044912E
+apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 4B4E7A9523ACD201
+apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys EFDC8610341D9410
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F24AEA9FB05498B7
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 818A435C5FCBF54A
+apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 3766223989993A70
+apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys E58A9D36647CAE7F
+apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 2CC26F777B8B44A1
