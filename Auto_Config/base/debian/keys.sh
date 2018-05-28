54ef684a4383bb23f902fda1133574aa9d6d50af
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-05-28 01:36:44 -0300

    debian stable

--- Auto_Config/base/debian/keys.sh
+++ Auto_Config/base/debian/keys.sh
@@ -1,10 +1,12 @@
-############ KEYS
+#### keys
+curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
 apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys FC918B335044912E
 apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 4B4E7A9523ACD201
 apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys EFDC8610341D9410
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F24AEA9FB05498B7
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
-apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 818A435C5FCBF54A
-apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 3766223989993A70
-apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys E58A9D36647CAE7F
+apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 74A941BA219EC810
+wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 2CC26F777B8B44A1
+apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 818A435C5FCBF54A
