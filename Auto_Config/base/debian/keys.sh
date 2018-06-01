db52d23637f2e34c9d9a4a97b072271215b29c5d
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-06-01 00:13:06 -0300

    repositorios/chaves melhoradas - testing

--- Auto_Config/base/debian/keys.sh
+++ Auto_Config/base/debian/keys.sh
@@ -1,12 +1,12 @@
 #### keys
-curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
-apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys FC918B335044912E
-apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 4B4E7A9523ACD201
+## spotify
 apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys EFDC8610341D9410
+
+## steam
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F24AEA9FB05498B7
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
-apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 74A941BA219EC810
-wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
-wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+
+## vivaldi
 apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 2CC26F777B8B44A1
-apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 818A435C5FCBF54A
+
+## mega
+apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 4B4E7A9523ACD201
