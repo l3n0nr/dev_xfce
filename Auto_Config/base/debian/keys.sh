9b7a73d0e4e48ea901ec52085cef486554631a34
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-06-12 20:01:09 -0300

    vivaldi removido

--- Auto_Config/base/debian/keys.sh
+++ Auto_Config/base/debian/keys.sh
@@ -5,8 +5,5 @@ apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys EFDC8610341D9410
 ## steam
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys F24AEA9FB05498B7
 
-## vivaldi
-apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 2CC26F777B8B44A1
-
 ## mega
 apt-key adv --keyserver ha.pool.sks-keyservers.net --recv-keys 4B4E7A9523ACD201
