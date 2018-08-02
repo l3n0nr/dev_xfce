c9f074231f465bc86ba8519dffc67bb488d6dc04
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-08-02 13:55:21 -0300

    arquivo modificado - verificar

--- Auto_Config/v5.sh
+++ Auto_Config/v5.sh
@@ -1373,7 +1373,7 @@ func_help()
         	printf "\n[*] Instalando o Docker"
             printf "\n[*] Instalando o Docker" >> $arquivo_log
     
-            curl -fsSL https://get.docker.com/ | sh                
+            curl -fsSL https://get.docker.com/ | sh
         else
             printf "\n[-] O Docker já está instalado no seu sistema."
             printf "\n[-] O Docker já está instalado no seu sistema." >> $arquivo_log
