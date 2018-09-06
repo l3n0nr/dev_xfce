3482942078f5e8d2951ef0ca7533b90513c45559
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-09-05 22:09:33 -0300

    arquivo otimizado

--- Auto_Config/v5_gui.sh
+++ Auto_Config/v5_gui.sh
@@ -1,13 +1,24 @@
-#!/bin/bash
-
+#!/usr/bin/env bash
+#
 cd /home/lenonr/Github/dev_xfce/Auto_Config
-
-if [[ $? -eq 0 ]]; then
-	su root v5.sh interface
-
+#
+interface()
+{
 	if [[ $? -eq 0 ]]; then
-		exit 0
-	else
-		exit 1
+		su root v5.sh interface
+
+		if [[ $? -eq 0 ]]; then
+			exit 0
+		else
+			exit 1
+		fi
 	fi
-fi
+}
+
+main()
+{
+	interface
+}
+
+# chamando funcao principal
+main
