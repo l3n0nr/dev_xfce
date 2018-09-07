5bf8a0e89d1eeb5482da4a1cf6499d0b2e81352c
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-09-07 10:47:21 -0300

    arquivo melhorado

--- main.sh
+++ main.sh
@@ -2,4 +2,34 @@
 #
 # Arquivo de referencia
 cd Auto_Config
-source v5.sh
+
+interface()
+{
+	if [[ $? -eq 0 ]]; then
+		su root v5.sh interface
+
+		if [[ $? -eq 0 ]]; then
+			exit 0
+		else
+			exit 1
+		fi
+	fi
+}
+
+default()
+{
+	source v5.sh
+}
+
+main()
+{
+	if [[ $1 == "interface" ]]; then
+		interface	
+	else
+		default
+	fi
+	
+}
+
+# chamando funcao principal
+main $1
