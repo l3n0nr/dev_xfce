2a18cb3d68374b2aa3541e5cfb92f46f86bddce3
Lenon Ricardo <lenonrmsouza@gmail.com>
2018-10-14 09:43:40 -0300

    bug language corrigido

--- Auto_Config/v5.sh
+++ Auto_Config/v5.sh
@@ -429,7 +429,7 @@ func_help()
         # fi
 
         [[ $(tail -1 /etc/locale.gen | grep $language) = "" ]] \
-            && echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+            && echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen \
             || printf "\n\n[-] Configuracao do idioma ja realizada!"
 
         if [[ $distro = "Debian" ]]; then
